616fafa922f5c7ceebb068085d019730b470464c
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-09-06 17:04:12 -0600

    Visibility script

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,10 +3,28 @@ var DailyWeather = $("#HrlyWeather");
 var Forecast = $("#weather")
 var cityURL = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={apiKey}'
 var newCity;
+var searchedArr = JSON.parse(loacalStorage.getItem('searchedItems')) || [];
+
+
+var city;
+var temp;
+var humidity;
+var wind;
+var index;
+var icon;
 
 $(document).ready(function() {
     function displaylist() {
-        $('#searchList')
+        $('#searchNew').empty();
+        if (searchedArr.length > 0){
+            document.getElementById("clear").style.visibility = "visiblle";
+            searchedArr.forEach(function(val) {
+                var li = $('<li>').attr('class', 'list-group-item');
+                li.text(val);
+                $('#searchNew').append(li);
+            }) 
+        } else{
+            document.getElementById("clear").style.visibility = "hidden";
+        }
     }
-}
-if ()
+    
