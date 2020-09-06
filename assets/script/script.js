1ff47a0a86b6790cca55f58f8c8fda13f539e8b5
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-09-06 16:17:16 -0600

    Script apiKey

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+const apiKey = "ae7db87635d2ab4e4912aa3633bda4f0"
+var DailyWeather = $("#HrlyWeather");
+var Forecast = $("#weather")
+var cityURL = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={apiKey}'
+var newCity;
+
+$(document).ready(function() {
+    function displaylist() {
+        $('#searchList')
+    }
+}
+if ()
